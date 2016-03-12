87aecb58e0be032e1e08d6628c5c350da2b30180
Bill Marcy <william_marcy@brown.edu>
2016-03-12 18:08:02 -0500

    Added file classes

---
+++ src/main/java/edu/brown/cs/h2r/burlapcraft/dungeongenerator/FileDungeon.java
@@ -0,0 +1,73 @@
+package edu.brown.cs.h2r.burlapcraft.dungeongenerator;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import cern.colt.Arrays;
+import edu.brown.cs.h2r.burlapcraft.helper.HelperGeometry.Pose;
+import net.minecraft.block.Block;
+import net.minecraft.world.World;
+
+public abstract class FileDungeon extends Dungeon {
+	
+	private static final String DIRECTORY = "dungeons/"; 
+	
+	protected static class DungeonMap {
+		private int [][][] map;
+		
+		public DungeonMap(int length, int width, int height) {
+			map = new int[height][length][width];
+		}
+		
+		public int get(int x, int y, int z) {
+			return map[y][z][x];
+		}
+		
+		public void set(int block, int x, int y, int z) {
+			map[y][z][x] = block;
+		}
+	}
+	
+	String filename;
+	// whenever you add a block to the world from an initial pose
+	// with an offset dx, dy, dz, call map.set(blockid, dx, dy, dz)
+	DungeonMap map;
+	
+	public FileDungeon(String filename, Pose _pose) throws IOException {
+		super(filename.split("\\.")[0], _pose,
+				Integer.parseInt(filename.split("\\.")[1].split("x")[2]),
+				Integer.parseInt(filename.split("\\.")[1].split("x")[0]),
+				Integer.parseInt(filename.split("\\.")[1].split("x")[1]),
+				Pose.fromXyz(1, 1, 3));
+
+		this.filename = filename;
+		
+		map = new DungeonMap(getLength(), getWidth(), getHeight());
+	}
+	
+	public void write() throws IOException {
+		
+		StringBuilder s = new StringBuilder();
+		for (int i = 0; i < getHeight(); i++) {
+			for (int j = 0; j < getLength(); j++) {
+				for (int k = 0; k < getLength(); k++) {
+					s.append(map.get(k, i, j));
+					s.append(" ");
+				}
+				s.append("\n");
+			}
+			s.append("\n");
+		}
+		
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(DIRECTORY + filename)));
+		out.write(s.toString());
+		out.flush();
+		out.close();
+	}
+}
---
+++ src/main/java/edu/brown/cs/h2r/burlapcraft/dungeongenerator/ReadDungeon.java
@@ -0,0 +1,65 @@
+package edu.brown.cs.h2r.burlapcraft.dungeongenerator;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import edu.brown.cs.h2r.burlapcraft.helper.HelperGeometry.Pose;
+import net.minecraft.block.Block;
+import net.minecraft.world.World;
+
+public class ReadDungeon extends FileDungeon {
+	
+	public ReadDungeon(String filename, Pose _pose) throws IOException {
+		super(filename, _pose);
+		read();
+	}
+	
+	public void read() throws IOException {
+		BufferedReader r = new BufferedReader(new FileReader("dungeons/" + filename));
+		String cur = r.readLine();
+		
+		int y = 0;
+		int z = 0;
+		
+		while (cur != null) {
+			if (cur.equals("")) {
+				// if we encounter a blank line, this is a new xz-plane.
+				// Increment y and reset x and z.
+				y++;
+				z = 0;
+				
+				cur = r.readLine();
+				continue;
+			}
+			// otherwise, it's the next line on the current xz-plane.
+			String[] line = cur.split("\\s");
+			
+			int x = 0;
+			
+			for (int i = 0; i < line.length; i++) {
+				String token = line[i];
+				if (!token.isEmpty()) {
+					map.set(Integer.parseInt(token), x, y, z);
+					x++;
+				}
+			}
+			
+			z++;
+			cur = r.readLine();
+		}
+		r.close();
+	}
+	
+	@Override
+	protected void generate(World world, int x, int y, int z) {
+		// treat the given coordinate as the top left
+		for (int i = 0; i < getHeight(); i++) {
+			for (int j = 0; j < getLength(); j++) {
+				for (int k = 0; k < getWidth(); k++) {
+					world.setBlock(x + k, y - i, z + j, Block.getBlockById(map.get(k, i, j)));
+				}
+			}
+		}
+	}
+}
