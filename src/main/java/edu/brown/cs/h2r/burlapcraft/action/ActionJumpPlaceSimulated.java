60a19995458ac5f049c0a8d7e84280e6ea7ed077
James Laskey <james.k.laskey@gmail.com>
2016-04-01 17:59:09 -0400

    Revert "some work on sane state updates"
    This reverts commit ec7de593c188f2d5bd1ecfd5467c69e266764f8c.

--- src/main/java/edu/brown/cs/h2r/burlapcraft/action/ActionJumpPlaceSimulated.java
+++ src/main/java/edu/brown/cs/h2r/burlapcraft/action/ActionJumpPlaceSimulated.java
@@ -1,7 +1,5 @@
 package edu.brown.cs.h2r.burlapcraft.action;
 
-import net.minecraft.block.Block;
-import edu.brown.cs.h2r.burlapcraft.helper.HelperActions;
 import edu.brown.cs.h2r.burlapcraft.helper.HelperNameSpace;
 import edu.brown.cs.h2r.burlapcraft.stategenerator.StateGenerator;
 import burlap.oomdp.core.Domain;
@@ -21,21 +19,8 @@ public class ActionJumpPlaceSimulated extends SimpleDeterministicAction {
 		StateGenerator.validate(s);
 		//get agent and current position
 		ObjectInstance agent = s.getFirstObjectOfClass(HelperNameSpace.CLASSAGENT);
-		int curX = agent.getIntValForAttribute(HelperNameSpace.ATX);
 		int curY = agent.getIntValForAttribute(HelperNameSpace.ATY);
-		int curZ = agent.getIntValForAttribute(HelperNameSpace.ATZ);
-		
-		String blockKey = curX + "," + curY + "," + curZ;
-		String blockName = StateGenerator.blockNameMap.get(blockKey);
-		
-		ObjectInstance block = s.getObject(blockName);
-		int blockId = agent.getIntValForAttribute(HelperNameSpace.ATSELECTEDITEMID);
-		if (blockId != -1) {
-			//Agent has a block in hand to place
-			block.setValue(HelperNameSpace.ATBTYPE, blockId);
-		
-			agent.setValue(HelperNameSpace.ATY, curY + 1);
-		}
+		agent.setValue(HelperNameSpace.ATY, curY + 1);
 		StateGenerator.validate(s);
 		//return the state we just modified
 		System.out.println("\nActionJumpPlaceSimulated performActionHelper\n");
